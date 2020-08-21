7d8e3e3f5e17ac1ada1854db0d0c3fe02ccd82ad
unknown <GhaithBenAmor>
2020-08-21 19:41:34 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  image1:string ="assets/images/1.png";
+  image2:string ="assets/images/2.png";
+  image3:string ="assets/images/3.png";
+
+}
